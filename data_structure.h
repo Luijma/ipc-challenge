2069db18cec183c3b5ecf1f9a5d2ac2cdef6adf7
Luijma <rsluismanuel@gmail.com>
2024-05-30 18:07:21 -0400

    Added updated data_structure.h to work with synchronization

--- data_structure.h
+++ data_structure.h
@@ -1,6 +1,8 @@
 #ifndef DATA_STRUCTURE_H
 #define DATA_STRUCTURE_H
 
+#include <semaphore.h>
+
 typedef enum { type1, type2, type3 } DataType;
 
 typedef struct {
@@ -9,5 +11,11 @@ typedef struct {
     char stringValue[100];
     DataType enumValue;
 } DataStructure;
+
+typedef struct {
+    DataStructure data[10];
+    sem_t sem_client;  
+    sem_t sem_server; 
+} SharedMemory;
 
 #endif // DATA_STRUCTURE_H
